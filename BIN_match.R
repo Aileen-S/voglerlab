93d1ec808c4a062c8381987db3d1bfe0859d2a99
Aileen-S <a.scott21@imperial.ac.uk>
2024-06-04 13:31:40 +0100

    initial commit

---
+++ BIN_match.R
@@ -0,0 +1,39 @@
+library(bold)
+library(seqinr)
+library(dplyr)
+library(getopt)
+
+spec = matrix(c(
+  'input',  'i', 1, "character", "Input fasta",
+  'output', 'o', 1, "character", "Output CSV"
+), byrow=T, ncol=5)
+opt = getopt(spec)
+
+
+seqs <- read.fasta(opt$input)
+
+matches <- data.frame(matrix(ncol = 12, nrow = 0))
+matches <- matches %>% mutate(across(everything(), as.character))
+colnames(matches) <- c('db_id', 'BIN_match', 'ID', 'sequencedescription', 'database', 'citation', 
+                       'taxonomicidentification', 'similarity', 'specimen_url',
+                       'specimen_country', 'specimen_lat', 'specimen_lon')
+
+for (name in names(seqs)) {
+  seq <- paste(seqs[[name]], collapse = "")
+  bin_info <- bold_identify(seq)
+  match <- bin_info[[1]][1,]
+  match['db_id'] = name
+  match$similarity <- as.numeric(match$similarity)
+  if (is.na(match$similarity)) {
+    match['BIN_match'] = 'no'
+  } else {
+    if (match$similarity > 0.978) {
+      match['BIN_match'] = 'yes'
+    } else {
+      match['BIN_match'] = 'no'
+  }}
+  match <- match %>% mutate(across(everything(), as.character))
+  matches <- bind_rows(matches, match)
+}
+
+write.csv(matches, opt$output, row.names = FALSE)
