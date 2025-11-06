df36fc9ce32376b2a062ae55a589b5836baa4663
Aileen-S <a.scott21@imperial.ac.uk>
2025-11-06 17:46:40 +0000

    ignore ambiguous characters

--- remove_dups.py
+++ remove_dups.py
@@ -1,6 +1,53 @@
+#!/usr/bin/env python3
 
-import argparse
 from Bio import SeqIO
+from Bio.Seq import Seq
+import argparse
+import re
+
+
+def is_nucleotide(records):
+    acgt  = ['A', 'C', 'G', 'T']
+    nuc = 0
+    other = 0
+    for rec in records:
+        seq = rec.seq.upper().replace('-', '')
+        for base in seq:
+            if base in acgt:
+                nuc += 1
+            else:
+                other += 1
+    return True if nuc > other else False
+
+
+def replace_ambiguous(records, nuc):
+    ambiguous = '[RYSWKMBDHVN]' if nuc else '[BZJX]'
+    for rec in records:
+        seq = str(rec.seq.upper())
+        seq = re.sub(ambiguous, '-', seq)
+        rec.seq = Seq(seq)
+    return records
+
+
+def remove_identical(records):
+    sequences = []
+    seq_ids = []
+
+    for rec in records:
+        if rec.seq not in sequences:
+            sequences.append(rec.seq)
+            seq_ids.append(rec.id)
+
+    return seq_ids
+
+
+def is_identical_overlap(seq_a, seq_b, min):
+    overlap = [
+        (a, b) for a, b in zip(seq_a, seq_b)
+        if a != '-' and b != '-'
+    ]
+    if len(overlap) > min:
+        return all(a == b for a, b in overlap)
 
 
 # Argument parserremoved and 
@@ -9,96 +56,45 @@ parser = argparse.ArgumentParser(description="Remove duplicate sequences from fa
 parser.add_argument("-i", "--input", type=str, help="Input fasta")
 parser.add_argument('-o', '--output', type=str, help="Output fasta with duplicates removed")
 parser.add_argument('-d', '--dups', type=str, help="Optional output file for removed duplicate sequences")
-parser.add_argument('-m', '--min_overlap', type=int, help="Minumum overlap length for sequences to be compared and removed if identical")
-
+parser.add_argument('-m', '--min_overlap', type=int, help="Minumum overlap length for sequences to be compared and removed if identical (default 200 characters)")
 
 args = parser.parse_args()         
 
-sequences = []
-check = set()
-seq_ids = []
-dup_ids = []
 
-initial_count = 0
-# Remove identical sequences
-with open(args.input) as file:
-    record = SeqIO.parse(file, "fasta")
-    for rec in record:
-        initial_count += 1
-        if rec.seq not in sequences:
-            sequences.append(rec.seq)
-            check.add(rec.seq)
-            seq_ids.append(rec.id)
+def main():
+    # Remove identical sequences
+    with open(args.input) as file:
+        records = list(SeqIO.parse(file, "fasta"))
+
+    nuc = is_nucleotide(records)
+    clean = replace_ambiguous(records, nuc)
+    dedup = [rec for rec in records if rec.id in remove_identical(clean)]
+
+    final_ids = []
+    final_seqs = []
+
+    min_overlap = args.min_overlap if args.min_overlap else 200
+    for rec in dedup:
+        for s in final_seqs:
+            if is_identical_overlap(rec.seq, s, min_overlap):
+                break
         else:
-            dup_ids.append(rec.id)
-records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in seq_ids)
+            final_ids.append(rec.id)
+            final_seqs.append(rec.seq)
 
-# Check for sequences with identical overlapping potrion
-# New dict for sequences
-final_seqs = {}
-x = 0
+    output_records = [rec for rec in records if rec.id in final_ids]
 
-# Threshold for minimum overlap length 
-if args.min_overlap:
-    min_overlap_length = args.min_overlap
-else:
-    min_overlap_length = 200
+    with open(args.output, 'w') as output:
+        SeqIO.write(output_records, output, "fasta")
+    print(f"Saved {len(output_records)} unique records of {len(records)} from {args.input} to {args.output}")
 
-# Interrate throguh records
-for rec in records:
-    if x == 0:
-        # Add first sequence to final_seqs
-        final_seqs[rec.id] = rec.seq
-        x += 1
-        continue
+    if args.dups:
+        dups = [rec for rec in records if rec.id not in final_ids]
+        with open(args.dups, 'w') as output:
+            SeqIO.write(dups, output, "fasta")
+        print(f"{len(dups)} duplicate records saved to {args.dups}")
     else:
-        x += 1
-        for seq_id in list(final_seqs.keys()):  # Use list to avoid modifying dict while iterating
-            sequence_match = True
-            overlap_count = 0
-            for i, char in enumerate(rec.seq):
-                if char == '-' or final_seqs[seq_id][i] == '-':
-                    continue  # Skip gaps
-                elif char != final_seqs[seq_id][i]:
-                    sequence_match = False
-                    break  # Exit the loop on mismatch
-                else: 
-                    overlap_count += 1 
+        print(f"{len(records) - len(output_records)} duplicate records removed")
 
-            # Check if sequences have significant overlap
-            if overlap_count < min_overlap_length: 
-                sequence_match = False
-
-
-            if sequence_match:
-                # Count non-gap characters
-                print(f'{seq_id} identical to {rec.id}')
-                rec_non_gap_count = sum(c != '-' for c in rec.seq)
-                final_non_gap_count = sum(c != '-' for c in final_seqs[seq_id])
-                
-                if rec_non_gap_count > final_non_gap_count:
-                    # New sequence has fewer gaps, keep it
-                    dup_ids.append(seq_id)
-                    final_seqs[rec.id] = rec.seq
-                    del final_seqs[seq_id]
-                else:
-                    # Existing sequence has fewer gaps, add the new sequence ID to duplicates
-                    dup_ids.append(rec.id)
-                break  # Exit loop after handling match
-            else:
-                # No match found, add the sequence to final_seqs
-                final_seqs[rec.id] = rec.seq
-
-
-count = len(final_seqs)
-with open(args.output, 'w') as output:
-    for rec_id, seq in final_seqs.items():
-        output.write(f'>{rec_id}\n{seq}\n')
-print(f"Saved {count} unique records of {initial_count} from {args.input} to {args.output}")
-
-if args.dups:
-    dups = (r for r in SeqIO.parse(args.input, "fasta") if r.id in dup_ids)
-    count = SeqIO.write(dups, args.dups, "fasta")
-    print(f"{count} duplicate records saved to {args.dups}")
-else:
-    print(f"{len(dup_ids)} duplicate records removed")
+if __name__ == "__main__":
+    main()
